b37bc3b5418df0b7cf7098680cf895a14e746880
Han Chiang <yaphc@hotmail.com>
2018-11-11 17:26:27 +0800

    What the fuck is wrong with the indentation

--- controller.py
+++ controller.py
@@ -19,6 +19,7 @@ from pox.lib.addresses import IPAddr, EthAddr
 
 log = core.getLogger()
 
+
 class Controller(EventMixin):
     def __init__(self):
         self.listenTo(core.openflow)
@@ -27,9 +28,9 @@ class Controller(EventMixin):
         # Routing table for 4 switches: Dictionary of dictionary
         self.macToPort = {}
 
-    # You can write other functions as you need.
-        
-    def _handle_PacketIn (self, event):    
+        # You can write other functions as you need.
+
+    def _handle_PacketIn(self, event):
         packet = event.parsed
         inPort = event.port
 
@@ -37,49 +38,46 @@ class Controller(EventMixin):
         src = packet.src
         dst = packet.dst
 
-    	# install entries to the route table
+        # install entries to the route table
         def install_enqueue(event, packet, outport, q_id):
             pass
-          
 
-    	# Check the packet and decide how to route the packet
-        def forward(message = None):
-            log.debug("packet: %s, dpid: %s, src: %s, dst: %s, port: %s" %(packet, dpid, src, dst, port))
+        # Check the packet and decide how to route the packet
+
+        def forward(message=None):
+            log.debug("packet: %s, dpid: %s, src: %s, dst: %s, port: %s" % (packet, dpid, src, dst, port))
 
             if dst.is_multicast:
                 flood()
             elif dst not in self.macToPort[dpid]:
                 flood('Port for %s unknown -- flooding' % (dst)
             else:
-                pass
-                # outPort = self.macToPort[dpid][dst]
-                # if outPort == inPort:
-                    #log.warning('Same port for packet from %s -> %s on %s.%s. Drop' % (src, dst, dpid_to_str(dpid, outPort)))
-    
+                outPort = self.macToPort[dpid][dst]
+                if outPort == inPort:
+                    log.warning('Same port for packet from %s -> %s on %s.%s. Drop' % (src, dst, dpid_to_str(dpid, outPort)))
+
 
         # When it knows nothing about the destination, flood but don't install the rule
-        def flood (message = None):
-            pass
-        
-        
+        # def flood(message=None):
+
+
         forward()
 
 
     def _handle_ConnectionUp(self, event):
-        dpid = dpid_to_str(event.dpid)
+        dpid=dpid_to_str(event.dpid)
         log.debug("Switch %s has come up.", dpid)
 
         # Init routing table for each switch
-        self.macToPort[dpid] = {}
-        
+        self.macToPort[dpid]={}
+
         # Send the firewall policies to the switch
         def sendFirewallPolicy(connection, policy):
             pass
-            
 
         # for i in [FIREWALL POLICIES]:
            # sendFirewallPolicy(event.connection, i)
-            
+
 
 def launch():
     # Run discovery and spanning tree modules
